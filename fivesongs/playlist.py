389978e25ebc88c84cae2ec4658ffe52a03bb5b1
Barbara Shaurette <barbarashaurette@Barbaras-MacBook-Pro.local>
2025-03-29 09:25:01 -0700

    rss and mastodon icons

--- fivesongs/playlist.py
+++ fivesongs/playlist.py
@@ -1,6 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import json
 
+from feedgen.feed import FeedGenerator
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -62,3 +63,43 @@ def today():
     }
     output = json.loads(json.dumps(play))
     return output
+
+@bp.route('/rss')
+def rss():
+    fg = FeedGenerator() 
+    fg.id('https://www.fivesongsdaily.com')
+    fg.title('Five Songs Daily')
+    fg.author({'name':'Barbara','email':'barbara@mechanicalgirl.com'})
+    fg.link(href='https://www.fivesongsdaily.com', rel='alternate')
+    fg.logo('https://www.fivesongsdaily.com/static/images/lgo_main.png')
+    fg.subtitle('RSS feed from fivesongsdaily.com')
+    fg.link(href='https://www.fivesongsdaily.com', rel='self')
+    fg.language('en')
+
+    db = get_db()
+    posts_query = "SELECT play_date, song_list FROM playlist WHERE play_date <= current_date ORDER BY play_date DESC LIMIT 10"
+    posts = db.execute(posts_query).fetchall()
+    
+    entries = []
+    for p in posts:
+        entry = {}
+        entry['title'] = str(p['play_date'])
+        entry['body_snippet'] = p['song_list']
+        date_str = str(p['play_date'])
+        datetime_obj = datetime.strptime(date_str, '%Y-%m-%d')
+        datetime_obj = datetime_obj.replace(tzinfo=timezone.utc)
+        entry['date'] = datetime_obj
+        entries.append(entry)
+    
+    for e in entries:
+        fe = fg.add_entry(order="append")
+        fe.id()
+        fe.title(e['title'])
+        fe.link(href=f"https://www.fivesongsdaily.com")
+        fe.description(e['body_snippet'])
+        fe.published(e['date'])
+        
+    fg.rss_file('rss.xml')
+    rssfeed  = fg.rss_str(pretty=True)
+        
+    return rssfeed
